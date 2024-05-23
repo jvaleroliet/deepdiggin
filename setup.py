3d5ba2f91f3b9745d0ed318811294dcce5e28339
Juan Valero <olietvalero@gmail.com>
2024-05-24 01:17:37 +0200

    include setup and requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deepdiggin",
+    version="0.1",
+    author="Juan Valero",
+    packages=find_packages(),
+    install_requires=[
+        "transformers>=4.41.1"
+        ],
+)
